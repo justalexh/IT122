e418bf6b1420a6759f5c5d0bfe809249e84a95c5
justalexh <alexhuynh0017@outlook.com>
2023-10-03 22:27:26 -0700

    Added Week 1 Introduce

---
+++ week_1Introduce Yourself!/script.js
@@ -0,0 +1,5 @@
+//JS Script for description of myself. 
+const firstName = "Alex";
+const lastName = "Huynh";
+let yearsOfStudy = "6 months";
+let goal = "pursue a career in backend development or a techincal project manager role";
